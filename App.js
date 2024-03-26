61ec2600cce06d640769e846811ae667dbeef42b
Igor-alves2301 <igoralv23@gmail.com>
2024-03-25 21:38:27 -0300

    Avaliação

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+
+  const [altura, setAltura] = React.useState('');
+  const [base, setBase] = React.useState('');
+
+
+  const area = (parseFloat(altura) * parseFloat(base)) / 2;
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Altura:</Text>
+      <TextInput style={styles.caixaTexto}/>
+      <TextInput keyboardType='numeric'/>
+
+      <Text style={styles.field}>Base:</Text>
+      <TextInput style={styles.caixaTexto}/>
+      <TextInput keyboardType='numeric'/>
+
+      <Text style={styles.field}>Área do Triangulo:</Text>
+      <Text style={styles.result}>Resultado: {area}</Text>
+
+      
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -14,7 +37,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
   },
+  field:
+  {
+    color: '#black',
+    margin: '14px',
+  },
+  result:
+  {
+    color: '#red',
+    width: '30px',
+  }
 });
